e649dd686163870456227fec9a3ee4ddd3a90f7a
Tony Schappaugh <tschappaugh@page2studios.com>
2026-02-03 15:47:56 -0600

    Remove Build from Repo. Rework Sidebar Card Styles for Grid

--- app/public/wp-content/plugins/snap-events/build/blocks/events-grid/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-server-side-render'), 'version' => '2226460ec4b2c7b9b61f');
